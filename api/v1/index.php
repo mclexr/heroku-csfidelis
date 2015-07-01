3b0d6d3f78593d8ea43aa336a9fec0de028a1a92
Michael Fidelis <mclexr@gmail.com>
2015-07-01 18:41:19 -0300

    Base

--- api/v1/index.php
+++ api/v1/index.php
@@ -1,9 +1,9 @@
 <?php
 require_once "../../vendor/autoload.php";
 require_once "request_headers.php";
-//ini_set('display_errors',1);
-//ini_set('display_startup_erros',1);
-//error_reporting(E_ALL);
+ini_set('display_errors',1);
+ini_set('display_startup_erros',1);
+error_reporting(E_ALL);
 
 //Imports =======================================================
 use Flynsarmy\SlimMonolog\Log\MonologWriter;
